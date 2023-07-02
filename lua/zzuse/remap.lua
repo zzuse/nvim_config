4fdab3a9401b7857a11184e2509cc1a05335618b
zzuse <zzuse@outlook.com>
2023-07-01 23:59:10 -0500

    yank

--- lua/zzuse/remap.lua
+++ lua/zzuse/remap.lua
@@ -3,3 +3,9 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- deleted word won't go into clipboard
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+-- copy to system clipboard
+vim.keymap.set("n", "<leader>y", "\"+y")
