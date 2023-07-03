8755890fb283de47265d42611f21655d095c0c1d
zzuse <zzuse@outlook.com>
2023-07-02 23:05:33 -0500

    lsp

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -4,7 +4,24 @@ lsp.on_attach(function(client, bufnr)
   lsp.default_keymaps({buffer = bufnr})
 end)
 
--- (Optional) Configure lua language server for neovim
-require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+lsp.ensure_installed({
+    'tsserver',
+    'eslint',
+    'rust_analyzer',
+})
+
+-- completion C-p previous, C-n next
+local cmp = require('cmp')
+local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_mappings = lsp.defaults.cmp_mappings({
+    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+    ['<C-Space>'] = cmp.mapping.complete(),
+})
+
+lsp.setup_nvim_cmp({
+    mapping = cmp_mappings
+})
 
 lsp.setup()
--- lua/zzuse/remap.lua
+++ lua/zzuse/remap.lua
@@ -7,5 +7,15 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 -- deleted word won't go into clipboard
 vim.keymap.set("x", "<leader>p", "\"_dP")
 
--- copy to system clipboard
+-- copy to system clipboard, not only within vim
 vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+
+-- tmux session
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+
+-- replace abriviation
+vim.keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
+
+-- change file executable
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
